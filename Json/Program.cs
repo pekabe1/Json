4fe640f209b1e3d6ffe34bae607ff824dd1947e0
k.ziarkowski <k.ziarkowski@PK3-3>
2019-09-26 20:01:11 +0200

    Add project files.

---
+++ Json/Program.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+
+namespace Json
+{
+    class Program
+    {
+        Repository repo = new Repository();
+
+
+        //   var list repo.GetList();
+        static void Main(string[] args)
+        {
+            Repository repo = new repository();
+
+            var data = File.ReadAllText(ConfigurationManager.AppSettings["file"]);
+            //JObject jObject = JObject.Parse(data);
+            //Console.WriteLine("Name : " + jObject["name"]);
+
+
+
+
+
+
+            var personList = JsonConvert.DeserializeObject<List<Person>>(data);
+        //    personList.RemoveAt(1);
+            string dataToSave = JsonConvert.SerializeObject(personList);
+            File.WriteAllText((ConfigurationManager.AppSettings["file"]), dataToSave);
+
+
+            repo.Add(new Person
+            {
+                LastName = "Zyhu",
+                Name = "Ahdi",
+                listOfObjects = new List<string> { "1", "2", "abc" }
+            }); ; ;
+
+            string saveNewPerson = JsonConvert.SerializeObject(repo);
+            File.WriteAllText(ConfigurationManager.AppSettings["file"], saveNewPerson);
+
+
+           
+            Console.Read();
+
+
+
+        }
+    }
+}
+
+
---
+++ Json/Repository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Configuration;
+using Newtonsoft.Json;
+
+namespace Json
+{
+    public class Repository
+    {
+        //readonly string path = File.ReadAllText(ConfigurationManager.AppSettings["file"]);
+        List<Person> personList = JsonConvert.DeserializeObject<List<Person>>("Data.json") ;
+
+        public void Add(Person person)
+        {
+
+
+            personList.Add(new Person
+            {
+                LastName = person.LastName,
+                Name = person.Name,
+                listOfObjects = person.listOfObjects
+            });
+        }
+
+        internal List<Person> GetPerson()
+        {
+            return personList;
+
+
+        }
+    }
+}
